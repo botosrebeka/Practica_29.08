91a0cb9f1ab8af6386aa1ed3f081a34aa65a2403
Botos Rebeka <botosrebekaagnes@yahoo.com>
2023-09-08 17:15:00 +0300

    Predict

--- Predict.py
+++ Predict.py
@@ -3,21 +3,10 @@ from torch.utils.data import DataLoader
 import main
 import UNet
 import soundfile as sf
-
-
-valid_voice_annotation_file = "voice_valid_dataset.csv"
-valid_noise_annotation_file = "noise_valid_dataset.csv"
-valid_voice_dir = "E:/Practica/voice"
-valid_noise_dir = "E:/Practica/noise"
-
-valid_dataset = main.CustomSoundDataset(valid_voice_annotation_file, valid_noise_annotation_file, valid_voice_dir, valid_noise_dir)
-
-print(f"There are {len(valid_dataset)} samples in the valid dataset.")
-
-valid_dataloader = DataLoader(valid_dataset, batch_size=32, shuffle=False)
+import numpy as np
 
 model = UNet.UNetModel()
-model.load_state_dict(torch.load("model1.pth"))
+model.load_state_dict(torch.load("model_51.pth"))
 
 voice_annotation_file = "voice.csv"
 noise_annotation_file = "noise.csv"
@@ -28,8 +17,7 @@ csd = main.CustomSoundDataset(voice_annotation_file, noise_annotation_file, voic
 
 print(f"There are {len(csd)} samples in the dataset.")
 
-train_dataloader = DataLoader(csd, batch_size=4, shuffle=True)
-
+train_dataloader = DataLoader(csd, batch_size=2, shuffle=True)
 
 for X, y in train_dataloader:
     print(f"Shape of X: {X.shape}")
@@ -38,10 +26,10 @@ for X, y in train_dataloader:
     model.eval()
 
     with torch.no_grad():
-        asd = torch.squeeze(X)
-        asd = asd[1, :]
-        sf.write('input.wav', asd, 48000)
-        
+        asd = torch.squeeze(X, 1)
+        asd = asd[0, :]
+        sf.write('51_predict_input.wav', asd, 48000)
+
         X = main.transform(X)
         inp = X
         X = main.compute_magnitude(X)
@@ -51,12 +39,19 @@ for X, y in train_dataloader:
 
         pred = inp * masc
 
-        pred = torch.squeeze(pred)
-        
-        pred = pred[1, :, :, :]
-        invers = main.inverse(pred)
-        a = 1
+        zgomot = inp - pred
+        zgomot = torch.squeeze(zgomot, 1)
+        zgomot = zgomot[0, :, :, :]
+        inv = main.inverse(zgomot)
+        sf.write('51_zgomot.wav', inv, 48000)
 
-        sf.write('out.wav', invers, 48000)
+        pred = torch.squeeze(pred, 1)
+
+        pred = pred[0, :, :, :]
+        invers = main.inverse(pred)
+
+        sf.write('51_predict_output.wav', invers, 48000)
 
     break
+
+
