84efd76b60c46728c0a62e64006df037b8fcf5fc
Botos Rebeka <botosrebekaagnes@yahoo.com>
2023-09-08 17:14:29 +0300

    UNet

--- UNet.py
+++ UNet.py
@@ -1,15 +1,9 @@
 import torch
 from torch import nn
 
-# # Get cpu, gpu or mps device for training.
-# device = (
-#     "cuda"
-#     if torch.cuda.is_available()
-#     else "mps"
-#     if torch.backends.mps.is_available()
-#     else "cpu"
-# )
-# print(f"Using {device} device")
+device = "cpu"
+
+print(f"Using {device} device")
 
 
 class UNetModel(nn.Module):
@@ -44,32 +38,40 @@ class UNetModel(nn.Module):
         self.output = nn.Sigmoid()
 
     def forward(self, x):
-        # B, F, T = x.shape
-        # x = x.view((B, 1, F, T))
         x = torch.nn.functional.pad(x, (0, 22), 'constant', 0)
         x = x[:, :, :-1, :]
 
-        x1 = self.s1(x)
-        x2 = self.s2(x1)
-        x3 = self.s3(x2)
-        x4 = self.s4(x3)
+        bridge = []
 
-        b = self.b(x4)
+        x = self.s1(x)
+        bridge.append(x)
+        x = self.s2(x)
+        bridge.append(x)
+        x = self.s3(x)
+        bridge.append(x)
+        x = self.s4(x)
+        bridge.append(x)
+
+        b = self.b(x)
 
         x = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2, padding=0)(b)
-        x = torch.cat([x, x4], dim=1)
+        z = bridge.pop()
+        x = torch.cat([x, z], dim=1)
         x = self.d1(x)
 
         x = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2, padding=0)(x)
-        x = torch.cat([x, x3], dim=1)
+        z = bridge.pop()
+        x = torch.cat([x, z], dim=1)
         x = self.d2(x)
 
         x = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2, padding=0)(x)
-        x = torch.cat([x, x2], dim=1)
+        z = bridge.pop()
+        x = torch.cat([x, z], dim=1)
         x = self.d3(x)
 
         x = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2, padding=0)(x)
-        x = torch.cat([x, x1], dim=1)
+        z = bridge.pop()
+        x = torch.cat([x, z], dim=1)
         x = self.d4(x)
 
         x = nn.ConvTranspose2d(64, 1, kernel_size=2, stride=2, padding=0)(x)
@@ -78,14 +80,16 @@ class UNetModel(nn.Module):
 
         x = x[:, :, :, :-22]
 
-        x = torch.nn.functional.pad(x, (0, 0, 0, 1), 'replicate')
+        x = torch.cat((x, x[:, :, -1, :].unsqueeze(2)), dim=2)
+
+        # x = torch.nn.functional.pad(x, (0, 0, 0, 1), 'replicate')
 
         return x
 
 
 # model = UNetModel()
-# 
-# a = torch.randn(1, 513, 938)
+#
+# a = torch.randn(1, 1, 513, 938)
 # # x = torch.nn.functional.pad(a,  (0, 0, 0, 1), 'constant', 0)
 # # a = 1
 # model(a)
