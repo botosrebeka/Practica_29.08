ede13c7af638e2dcef5a0e32e7df46c4fb90d42a
Botos Rebeka <botosrebekaagnes@yahoo.com>
2023-09-08 17:13:03 +0300

    Modif

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from torch.utils.data import DataLoader
 import UNet
 import torch.nn as nn
 import torch.optim as optim
+import matplotlib.pyplot as plt
 
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
@@ -67,7 +68,7 @@ class CustomSoundDataset(Dataset):
     # lungimea datasetului = nr. de samples = 1000
     def __len__(self):
         # return len(self.voice_annotations)
-        return 30
+        return 50
 
     def __getitem__(self, index):
         # seed - hogy mindig azokat hasznald
@@ -78,24 +79,34 @@ class CustomSoundDataset(Dataset):
         self.voice_random_index = voice_state.randint(0, len(self.voice_annotations))
         self.noise_random_index = noise_state.randint(0, len(self.noise_annotations))
 
-        voice_sample_path = self._get_voice_sample_path(index)
-        noise_sample_path = self._get_noise_sample_path(index)
+        voice_sample_path = self._get_voice_sample_path()
+        noise_sample_path = self._get_noise_sample_path()
 
         voice, vsr = sf.read(voice_sample_path, dtype='float32', always_2d=True)
         noise, nsr = sf.read(noise_sample_path, dtype='float32', always_2d=True)
 
+        voice_norm = self.normalize(voice, -24)
+        noise_norm = self.normalize(noise, -30)
+
         # niste exemple sunt mai lunga decat 480000 sample -> slicing
-        voice_plus_noise = voice[0:480000, :] + noise[0:480000, :]
+        # noise = noise/3
+        voice_plus_noise = voice_norm[0:480000, :] + noise_norm[0:480000, :]
 
-        return voice_plus_noise.T, voice[0:480000, :].T
+        return voice_plus_noise.T, voice_norm[0:480000, :].T
 
-    def _get_voice_sample_path(self, index):
+    def _get_voice_sample_path(self):
         path = os.path.join(self.voice_dir, self.voice_annotations.iloc[self.voice_random_index, 0])
         return path
 
-    def _get_noise_sample_path(self, index):
+    def _get_noise_sample_path(self):
         path = os.path.join(self.noise_dir, self.noise_annotations.iloc[self.noise_random_index, 0])
         return path
+
+    def normalize(self, signal, db):
+        r = 10**(db/20.0)
+        a = np.sqrt((len(signal) * r**2) / np.sum(signal**2))
+
+        return signal * a
 
 
 # functie pentru a modifica lungimea
@@ -137,17 +148,19 @@ def transform(signal):
     hop_length = int(n_fft/2)
     win_length = n_fft
     # print(f"Shape before: {signal.shape}")
-    signal = torch.squeeze(signal)
+    # signal = torch.squeeze(signal)
+    signal = torch.squeeze(signal, 1)
 
     spectrogram = torch.stft(
         signal,
         n_fft=n_fft,
         hop_length=hop_length,
         win_length=win_length,
-        window=torch.hann_window(window_length=win_length),
+        window=torch.hann_window(window_length=win_length).to(device),
         return_complex=True
     )
-    # plot_spectrogram(spectrogram)
+    # sp = torch.squeeze(spectrogram)
+    # plot_spectrogram(sp)
     spectrogram = torch.view_as_real(spectrogram)
     spectrogram = torch.unsqueeze(spectrogram, 1)
     return spectrogram
@@ -165,7 +178,7 @@ def inverse(spectrogram):
         n_fft=n_fft,
         hop_length=hop_length,
         win_length=win_length,
-        window=torch.hann_window(window_length=win_length),
+        window=torch.hann_window(window_length=win_length).to(device),
         return_complex=False,
         length=480000
     )
@@ -189,10 +202,10 @@ def max_error(a, b):
 
 
 def compute_magnitude(complex_signal):
-    magnitude = torch.sqrt(complex_signal[:, :, :, :, 0] ** 2 + complex_signal[:, :, :, :, 1] ** 2)
-    # magnitude = torch.sqrt(complex_signal[:, :, 0] ** 2 + complex_signal[:, :, 1] ** 2)
-
+    energ = complex_signal[:, :, :, :, 0] ** 2 + complex_signal[:, :, :, :, 1] ** 2
+    magnitude = torch.sqrt(energ.clamp_min(1e-17))
     return magnitude
+
 
 # # Get cpu, gpu or mps device for training.
 # device = (
@@ -202,66 +215,42 @@ def compute_magnitude(complex_signal):
 #     if torch.backends.mps.is_available()
 #     else "cpu"
 # )
-# print(f"Using {device} device")
+device = "cpu"
+print(f"Using {device} device")
+
+loss_list = []
 
 
 def train(model, dataloader, loss_fn, optimizer):
+    num_batches = len(dataloader)
     model.train()
-    i = 0
+    epoch_loss = 0
     for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
         optimizer.zero_grad()
 
-        i += 1
-        # inf = str(i) + 'inp.wav'
-        ouf = str(i) + 'outp.wav'
+        X_trans = transform(X)
+        y_trans = transform(y)
 
-        # asd = torch.squeeze(X)
-        # asd = asd[0, :]
-        # sf.write(inf, asd, 48000)
+        fea = compute_magnitude(X_trans)
 
-        X = transform(X)
-        inp = X
-        X = compute_magnitude(X)
+        masc = model(fea)
 
-        masc = model(X)
-        masc = torch.unsqueeze(masc, -1)
+        pred = X_trans * masc.unsqueeze(-1)
 
-        pred = inp * masc
-        with torch.no_grad():
-            out = torch.squeeze(pred)
-
-            out = out[0, :, :, :]
-            invers = inverse(out)
-
-            sf.write(ouf, invers, 48000)
-
-        y = transform(y)
-
-        loss = loss_fn(pred, y)
-        print(f'loss:{loss.item()}')
-
+        loss = loss_fn(pred, y_trans)
         loss.backward()
         optimizer.step()
-    print("Epoch done!")
+
+        loss_list.append(loss.item())
+        epoch_loss += loss
+
+        print(f'Batch: {batch+1}/{num_batches}')
+
+    print(f'Epoch loss: {epoch_loss/num_batches}')
 
 
 if __name__ == "__main__":
-    # voice_annotation_file = "voice_train_dataset.csv"
-    # noise_annotation_file = "noise_train_dataset.csv"
-    # voice_dir = "E:/Practica/voice"
-    # noise_dir = "E:/Practica/noise"
-    #
-    # csd = CustomSoundDataset(voice_annotation_file, noise_annotation_file, voice_dir, noise_dir)
-    #
-    # print(f"There are {len(csd)} samples in the dataset.")
-    #
-    # train_dataloader = DataLoader(csd, batch_size=4, shuffle=True)
-    #
-    # for X, y in train_dataloader:
-    #     print(f"Shape of X: {X.shape}")
-    #     print(f"Shape of y: {y.shape}")
-    #     break
-
     voice_annotation_file = "voice.csv"
     noise_annotation_file = "noise.csv"
     voice_dir = "E:/Practica/voice"
@@ -271,59 +260,48 @@ if __name__ == "__main__":
 
     print(f"There are {len(csd)} samples in the dataset.")
 
-    train_dataloader = DataLoader(csd, batch_size=4, shuffle=True)
+    train_dataloader = DataLoader(csd, batch_size=10, shuffle=False)
 
     for X, y in train_dataloader:
         print(f"Shape of X: {X.shape}")
         print(f"Shape of y: {y.shape}")
         break
 
-    # model = UNet.UNetModel().to(device)
-    model = UNet.UNetModel()
+    model = UNet.UNetModel().to(device)
+    # model = UNet.UNetModel()
     loss_fn = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    epochs = 5
+    optimizer = optim.Adam(model.parameters(), lr=0.00001, weight_decay=0.000001)
+    epochs = 20
 
     for t in range(epochs):
         print(f"Epoch {t + 1}\n-------------------------------")
         train(model, train_dataloader, loss_fn, optimizer)
+        torch.save(model.state_dict(), "model_51.pth")
+        print("Saved PyTorch Model State to model_51.pth")
     print("Train done!")
 
-    torch.save(model.state_dict(), "model1.pth")
-    print("Saved PyTorch Model State to model1.pth")
+    x = np.linspace(0, len(loss_list), len(loss_list))
+    y = loss_list
 
-# for X, y in train_dataloader:
-#     print(f"Shape of X: {X.shape}")
-#     print(f"Shape of y: {y.shape}   v
-# 
-#     before = X[0, :, :]
-#     export('before', torch.squeeze(before, 0))
-# 
-#     x = X[0, :, :]
-#     x = transform(x)
-# 
-#     a = compute_magnitude(x)
-# 
-#     after = inverse(x)
-# 
-#     export('after', after)
-# 
-#     after = torch.unsqueeze(after, 0)
-#     print(f"Shape after: {after.shape}")
-# 
-#     print(f"Max error: {max_error(before, after)}")
-#     break
+    plt.plot(x, y)
+    plt.show()
 
-# for i in range(5):
-#     m, v = csd[i]
-#     wave_plot(m, v)
+    # for X, y in train_dataloader:
+    #     print(f"Shape of X: {X.shape}")
+    #     print(f"Shape of y: {y.shape}")
+    #
+    #     x = X[0, :, :]
+    #     x = transform(x)
+    #
+    #     # a = compute_magnitude(x)
+    #
+    #     # after = inverse(x)
+    #     #
+    #     # export('after', after)
+    #     #
+    #     # after = torch.unsqueeze(after, 0)
+    #     # print(f"Shape after: {after.shape}")
+    #     #
+    #     # print(f"Max error: {max_error(before, after)}")
+    #     break
 
-# model = UNet.UNetModel()
-# batch = next(iter(train_dataloader))
-# x, y = batch
-# x = transform(x)
-# magn = compute_magnitude(x)
-# magn.to(UNet.device)
-#
-#
-# model(magn)
